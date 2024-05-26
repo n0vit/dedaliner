b833c6314db10a60ffb6befcbade06308d51f5f3
Nickolas <kolzazanovikov@gmail.com>
2024-05-26 19:03:34 +0700

    Initial Commit

---
+++ Dedaliner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dedaliner
+//
+//  Created by NIKOLAI on 26.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
